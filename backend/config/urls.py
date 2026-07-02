2a0c06597ccfe797ca4752a06dafa42fe6cb6750
焦嬌 <jiaojiao@jiaojiaodeMac-mini.local>
2026-07-02 21:16:38 +0900

    chore: add docker deployment setup

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -14,5 +14,4 @@ urlpatterns = [
     path('api/v1/portal/', include('api.portal.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
